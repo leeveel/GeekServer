f555e0c0367db05ea90aea849f75b02be8fb18ab
Quan <quange0314@163.com>
2021-10-21 17:03:22 +0800

    修正mac下 文件夹分隔符问题

--- Tools/MessageGen/msg.bat
+++ Tools/MessageGen/msg.bat
@@ -1,2 +1,2 @@
-StateGenTool.exe %cd%\template\MsgFactory.template %cd%\template\message.template %cd%\Messages %cd%\..\..\GeekServer.Generate\Generate\Messages\ false
+StateGenTool.exe %cd%\template\MsgFactory.template %cd%\template\message.template %cd%\Messages %cd%\..\..\GeekServer.Generate\Generate\Messages false
 pause
--- Tools/MessageGen/msg_client.bat
+++ Tools/MessageGen/msg_client.bat
@@ -1,2 +1,2 @@
-StateGenTool.exe %cd%\template_client\MsgFactory.template %cd%\template_client\message.template %cd%\Messages %cd%\..\..\UnityDemo\Assets\Scripts\Generate\Messages\ false
+StateGenTool.exe %cd%\template_client\MsgFactory.template %cd%\template_client\message.template %cd%\Messages %cd%\..\..\UnityDemo\Assets\Scripts\Generate\Messages false
 pause
