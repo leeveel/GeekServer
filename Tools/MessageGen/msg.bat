3f2196f51be3b4af94b7a3a25c402508d72d4780
leeveel <406351314@qq.com>
2022-03-28 16:00:37 +0800

    新的协议工具GeekProto

--- Tools/MessageGen/msg.bat
+++
@@ -1,2 +0,0 @@
-StateGenTool.exe %cd%\template\MsgFactory.template %cd%\template\message.template %cd%\Messages %cd%\..\..\GeekServer.Generate\Generate\Messages false
-pause
--- Tools/MessageGen/msg_client.bat
+++
@@ -1,2 +0,0 @@
-StateGenTool.exe %cd%\template_client\MsgFactory.template %cd%\template_client\message.template %cd%\Messages %cd%\..\..\UnityDemo\Assets\Scripts\Generate\Messages false
-pause
