9425d02a5b3453879b0e21bc402b761a8cc83ca2
leeveel <406351314@qq.com>
2022-11-30 19:18:11 +0800

    整理starup

--- Geek.Server.App/Common/AppSetting.cs
+++ Geek.Server.App/Common/AppSetting.cs
@@ -5,9 +5,6 @@ namespace Geek.Server.App.Common
     public class AppSetting : BaseSetting
     {
         public readonly List<int> Servers = new();
-
-        public bool AllowOpen { get; set; } = true;
-
         public override bool IsLocal(int serverId)
         {
             return base.IsLocal(serverId) || Servers.Contains(serverId);
