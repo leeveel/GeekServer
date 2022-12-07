a755084a2b82ee1eb82abf03707fb2722e8edfa9
lixiuzhi <1642267742@qq.com>
2022-12-07 17:10:26 +0800

    tcp/httpserver app没有赋值

--- Geek.Server.Core/Net/Http/HttpServer.cs
+++ Geek.Server.Core/Net/Http/HttpServer.cs
@@ -8,7 +8,7 @@ namespace Geek.Server.Core.Net.Http
     public static class HttpServer
     {
         static readonly NLog.Logger Log = NLog.LogManager.GetCurrentClassLogger();
-        public static WebApplication WebApp { get; set; }
+        static WebApplication app { get; set; }
 
         /// <summary>
         /// 启动
@@ -40,7 +40,7 @@ namespace Geek.Server.Core.Net.Http
             })
             .UseNLog();
 
-            var app = builder.Build();
+            app = builder.Build();
             app.MapGet("/game/{text}", (context) => HttpHandler.HandleRequest(context));
             app.MapPost("/game/{text}", (context) => HttpHandler.HandleRequest(context));
             return app.StartAsync();
@@ -51,11 +51,11 @@ namespace Geek.Server.Core.Net.Http
         /// </summary>
         public static Task Stop()
         {
-            if (WebApp != null)
+            if (app != null)
             {
                 Log.Info("停止http服务...");
-                var task = WebApp.StopAsync();
-                WebApp = null;
+                var task = app.StopAsync();
+                app = null;
                 return task;
             }
             return Task.CompletedTask;
