64051af3efb571bf44ef4e9151641073e2f0c6fc
lixiuzhi <1642267742@qq.com>
2023-07-21 10:06:53 +0800

    更新协议项目

---
+++ Geek.Server.Proto/Proto/Message.cs
@@ -0,0 +1,12 @@
+﻿using MessagePack;
+
+[MessagePackObject(true)]
+public class Message
+{
+    /// <summary>
+    /// 消息唯一id
+    /// </summary>
+    public int UniId { get; set; }
+    [IgnoreMember]
+    public virtual int MsgId { get; }
+}
