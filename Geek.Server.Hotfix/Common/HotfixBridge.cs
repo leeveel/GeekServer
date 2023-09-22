9300f428a19c1053ff10dc0c3159cd1d177a4441
lixiuzhi <1642267742@qq.com>
2023-09-22 21:23:58 +0800

    停服添加try catch

--- Geek.Server.Hotfix/Common/HotfixBridge.cs
+++ Geek.Server.Hotfix/Common/HotfixBridge.cs
@@ -10,7 +10,7 @@ using Geek.Server.Core.Net.Websocket;
 using Geek.Server.Core.Timer;
 using Geek.Server.Core.Utils;
 using Microsoft.AspNetCore.Connections;
-using PolymorphicMessagePack; 
+using PolymorphicMessagePack;
 
 namespace Server.Logic.Common
 {
@@ -46,19 +46,29 @@ namespace Server.Logic.Common
 
         public async Task Stop()
         {
-            // 断开所有连接
-            await SessionManager.RemoveAll();
-            // 取消所有未执行定时器
-            await QuartzTimer.Stop();
-            // 保证actor之前的任务都执行完毕
-            await ActorMgr.AllFinish();
-            // 关闭网络服务
-            await HttpServer.Stop();
-            await TcpServer.Stop();
-            await WebSocketServer.Stop();
-            // 存储所有数据
-            await GlobalTimer.Stop();
-            await ActorMgr.RemoveAll();
+            try
+            {
+                // 断开所有连接
+                await SessionManager.RemoveAll();
+                // 取消所有未执行定时器
+                await QuartzTimer.Stop();
+                // 保证actor之前的任务都执行完毕
+                await ActorMgr.AllFinish();
+                // 关闭网络服务
+                await HttpServer.Stop();
+                await TcpServer.Stop();
+                await WebSocketServer.Stop();
+            }
+            catch (Exception e)
+            {
+                Log.Error(e);
+            }
+            finally
+            {
+                // 存储所有数据
+                await GlobalTimer.Stop();
+                await ActorMgr.RemoveAll();
+            }
         }
     }
 }
